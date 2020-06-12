6106b71d7ed2a60df740dd259e4c91ad0a45fd4f
MeiK <meik2333@gmail.com>
2020-06-12 16:56:00 +0800

    Update

--- android/qemud/Android.mk
+++ android/qemud/Android.mk
@@ -13,7 +13,8 @@ LOCAL_SRC_FILES:= \
 
 LOCAL_CFLAGS := \
     -Wno-unused-parameter \
-	-Wno-unused-variable
+	-Wno-unused-variable \
+	-Wno-unused-function
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils liblog
